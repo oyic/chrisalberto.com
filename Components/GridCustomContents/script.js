f6a32733f132e89eeacbd6b24cf7bc9ecca27ace
oyic <oyic@outlook.com>
2025-05-07 16:42:28 +0800

    added button link and adjust glare

--- Components/GridCustomContents/script.js
+++ Components/GridCustomContents/script.js
@@ -2,11 +2,12 @@ import VanillaTilt from 'vanilla-tilt'
 
 VanillaTilt.init(document.querySelectorAll('.tilt-me'), {
 
-  max: 30, // max tilt rotation (degrees)
+  max: 15, // max tilt rotation (degrees)
   startX: 0, // the starting tilt on the X axis, in degrees.
   startY: 0, // the starting tilt on the Y axis, in degrees.
   perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-  scale: 1,
+  scale: 1.05,
+  speed: 400,
   glare: true,
-  maxGlare: 0.1
+  "max-glare": 0.1,
 })
